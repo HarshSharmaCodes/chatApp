a18cb87b464dd96b96c8676d369b875771900edc
Harsh Sharma <hs0899728@gmail.com>
2025-01-22 18:40:02 +0530

    Added Routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
-const express = reuire ('express');
+import dotenv from "dotenv";
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-app.listen(5000,() => console.log('Server Running on port 5000'));
+dotenv.config();
+const PORT = process.env.PORT || 5000
+
+app.get("/",(req,res) => {
+    //root route http://localhost:5000/ 
+    res.send("Hello world!!");
+})
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT,() => console.log(`Server Running on port ${PORT}`));
