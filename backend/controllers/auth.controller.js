5f7dd8a598eca40902b8c8325cb0a094b13b218e
Harsh Sharma <hs0899728@gmail.com>
2025-01-22 23:46:12 +0530

    Changes Commmited

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,19 +1,23 @@
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
     try {
-        const { fullName, username, password, confirmPassword, gender } = req.body;
+        const { fullName, username, password, confirmPassword, gender} = req.body; 
 
         if (password !== confirmPassword) {
-            return res.status(400).json({ error: "Passwords Don't Match" });
-        }
-        const user = await User.findone({ username });
-        if (user) {
-            return res.status(400).json({ error: "Username Already Exists" });
+            return res.status(400).json({ error: "Passwords Don't Match" })
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10); 
+        const user = await User.findOne({username});
+
+        if (user) {
+            return res.status(400).json({ error: "Username Already Exists" })
+        }
+
+        //HashPasswordhere 
+        const pw = await bcrypt.genSalt(10);
+        const HashPassword = await bcrypt.hash(password,pw);
 
         const boyprofilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
         const girlprofilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -21,23 +25,25 @@ export const signup = async (req, res) => {
         const newUser = new User({
             fullName,
             username,
-            password:hashedPassword,
+            password: HashPassword,
             gender,
-            profilePic: gender === "male" ? boyprofilePic : girlprofilePic,
-        });
+            profilePic: gender === "male" ? boyprofilePic : girlprofilePic
+        })
 
         await newUser.save();
+        
         res.status(201).json({
-            id: newUser._id,
+            _id: newUser._id,
             fullName: newUser.fullName,
             username: newUser.username,
             profilePic: newUser.profilePic,
-        });
+        })
 
     } catch (error) {
+        console.log("Error in signUp Controller",error.message)
         res.status(500).json({ error: "Server Error" });
     }
-}
+};
 
 export const login = (req, res) => {
     console.log("Login user");
