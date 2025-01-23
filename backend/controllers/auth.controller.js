b7122f47bf6a18ad2b5ea048f960cbc1329dccee
Harsh Sharma <hs0899728@gmail.com>
2025-01-23 18:15:53 +0530

    Modifications commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,16 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generatetoken.js";
 
 export const signup = async (req, res) => {
     try {
-        const { fullName, username, password, confirmPassword, gender} = req.body; 
+        const { fullName, username, password, confirmPassword, gender } = req.body;
 
         if (password !== confirmPassword) {
             return res.status(400).json({ error: "Passwords Don't Match" })
         }
 
-        const user = await User.findOne({username});
+        const user = await User.findOne({ username });
 
         if (user) {
             return res.status(400).json({ error: "Username Already Exists" })
@@ -17,7 +18,7 @@ export const signup = async (req, res) => {
 
         //HashPasswordhere 
         const pw = await bcrypt.genSalt(10);
-        const HashPassword = await bcrypt.hash(password,pw);
+        const HashPassword = await bcrypt.hash(password, pw);
 
         const boyprofilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
         const girlprofilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -30,17 +31,20 @@ export const signup = async (req, res) => {
             profilePic: gender === "male" ? boyprofilePic : girlprofilePic
         })
 
-        await newUser.save();
-        
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic,
-        })
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            })
+        }
 
     } catch (error) {
-        console.log("Error in signUp Controller",error.message)
+        console.log("Error in signUp Controller", error.message)
         res.status(500).json({ error: "Server Error" });
     }
 };
