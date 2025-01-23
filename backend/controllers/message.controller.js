8c5adfc1460b5c3157585aee479dd2de3d5d909e
Harsh Sharma <hs0899728@gmail.com>
2025-01-24 00:53:59 +0530

    Modifications commit

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,11 @@
-export const sendMessage = async (req,res)=>
-{
+export const sendMessage = async (req, res) => {
     try {
-        
+        const { message } = req.body;
+        const { id:RecieverId } = req.params;
+        const senderId = req.user._id;
+
     } catch (error) {
-        
+        console.log("Error in Message Controller:", error.message)
+        res.status(500).json({ error: "Server Error" });
     }
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
 
+import cookieParser from "cookie-parser";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+	res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
