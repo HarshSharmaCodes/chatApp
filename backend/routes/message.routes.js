d2e314d27ea7896bf989a2be1003223295c332bc
Harsh Sharma <hs0899728@gmail.com>
2025-01-24 12:46:10 +0530

    Updated Routesr

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
-import protectRoute from "../middleware/protectroute.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, sendMessage)
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
