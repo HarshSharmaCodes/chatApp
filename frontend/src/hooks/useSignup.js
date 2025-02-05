5ad0be7d1da5145b2aee54111d22a4fa89e65de8
Harsh Sharma <hs0899728@gmail.com>
2025-02-05 12:32:21 +0530

    Added Hooks

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
     const [loading, setLoading] = useState(false);
+    const {setAuthUser} = useAuthContext();
 
     const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
         const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
@@ -11,14 +13,20 @@ const useSignup = () => {
         setLoading(true);
         try {
             const res = await fetch("api/auth/signup",
-            {
-                method: "POST",
-				headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ fullName, username, password, confirmPassword, gender }),
-            });
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ fullName, username, password, confirmPassword, gender }),
+                });
 
             const data = await res.json();
-            console.log(data);
+            if (data.error) {
+                throw new Error(data.error);
+            }
+
+            localStorage.setItem("chat-user",JSON.stringify(data));
+            setAuthUser(data);
+
         } catch (error) {
             toast.error(error.message);
         }
@@ -26,7 +34,7 @@ const useSignup = () => {
             setLoading(false);
         }
     };
-    
+
     return { loading, signup };
 };
 
