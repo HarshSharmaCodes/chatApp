31c947309ab8d4a9fc708f6358369a62731cd7c0
Harsh Sharma <hs0899728@gmail.com>
2025-02-19 14:57:00 +0530

    Added Socket in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 dotenv.config();
 
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -20,7 +22,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
