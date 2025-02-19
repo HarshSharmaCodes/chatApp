4f1866a33a6181303044c2f39b49a11bee0efe2f
Harsh Sharma <hs0899728@gmail.com>
2025-02-19 19:01:44 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Toaster } from "react-hot-toast";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { useAuthContext } from "./context/AuthContext";
-import Home from "./pages/home/Home";
+import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 
